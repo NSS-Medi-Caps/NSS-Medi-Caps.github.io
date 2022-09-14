c4dd19fc8fc23afd442c3f23193d323498fdaee1
vyom69 <en20cs301504@medicaps.ac.in>
2022-09-14 21:47:03 +0530

    event page updated

--- nss_medicaps/src/component/pages/Event.js
+++ nss_medicaps/src/component/pages/Event.js
@@ -1,8 +1,19 @@
 import '../assets/main.css';
 
 function Event() {
+
+
   return (
-    <div></div>
+    <>
+    <div>
+      <div class="leftBox">
+        <div class="content">
+          <h1>hello</h1>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </>
   );
 }
 
